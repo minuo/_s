c91066a5e04bc7523a950bdc21429955e7974ef5
minuo <815112098@qq.com>
2020-12-30 17:00:50 +0800

    a

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -1,18 +1,32 @@
 <header id="masthead" class="site-header">
-    <div class="site-header-main">
-        <div class="site-header-branding-menu">
-            <?php get_template_part('template-parts/header/site-branding'); ?>
-            <?php get_template_part('template-parts/header/site-nav'); ?>
-        </div>
-        <div class="site-header-search">
-            <form role="search" method="get" class="search-form" action="http://localhost/wpdev/">
-                <label>
-                    <span class="screen-reader-text">搜索：</span>
-                    <input type="search" class="search-field" placeholder="搜索…" value="" name="s">
-                </label>
-                <input type="submit" class="search-submit" value="搜索">
-            </form>
-        </div>
-    </div>
-    
-</header><!-- #masthead -->
+		<div class="site-branding">
+			<?php
+            the_custom_logo();
+            if (is_front_page() && is_home()) :
+                ?>
+				<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+				<?php
+            else :
+                ?>
+				<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+				<?php
+            endif;
+            $ibooks_description = get_bloginfo('description', 'display');
+            if ($ibooks_description || is_customize_preview()) :
+                ?>
+				<p class="site-description"><?php echo $ibooks_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', '_ibooks'); ?></button>
+			<?php
+            wp_nav_menu(
+                    array(
+                    'theme_location' => 'menu-1',
+                    'menu_id'        => 'primary-menu',
+                )
+                );
+            ?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
